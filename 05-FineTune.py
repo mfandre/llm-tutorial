f05cacbdd55b9e2faff3b1b8fc2dbd2bd5c03ab2
André de Mattos Ferraz <andre.mattos@eneva.com.br>
2024-11-25 11:35:48 -0300

    atualizando finetune

--- 05-FineTune.py
+++ 05-FineTune.py
@@ -9,9 +9,18 @@ OPENAI_SERVICE_ACCOUNT_KEY = os.getenv("OPENAI_SERVICE_ACCOUNT_KEY")
 # Configurar a chave da API
 client = OpenAI(api_key=OPENAI_SERVICE_ACCOUNT_KEY)
 
+# Subir o arquivo de treinamento
+file_obj = client.files.create(file=open("finetune.jsonl", "rb"), purpose="fine-tune")
 
-client.files.create(
-    file=open("finetune.jsonl", "rb"),
-    purpose="fine-tune"
+# Criar modelo fine-tuned
+job = client.fine_tuning.jobs.create(training_file=file_obj.id, model="davinci-002")
+print(job.fine_tuned_model)
+
+
+# Quando o job finalizar você poderá acessar o nome do modelo criar para ser usado...
+model_name = job.fine_tuned_model
+
+completion = client.chat.completions.create(
+    model=model_name, prompt="O que significa o código de erro E102?", max_tokens=100
 )
-
+print(completion.choices[0].message)
